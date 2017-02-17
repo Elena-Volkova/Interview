f06f608ab8d63d43d573729d585e39ca2d2dbac2
Elena Volkova <elena.volkova@oxagile.com>
2017-02-18 00:04:32 +0300

    Add 1-2 questions

---
+++ questions/src/question1/JavaExceptionQuestion.java
@@ -0,0 +1,15 @@
+package question1;
+
+import java.io.IOException;
+
+/**
+ * 1. Java exception hierarchy
+ * 2. Exmaples of checked/unchecked exceptions, errors?
+ * 3. Can unchecked exception be secified in throws section?
+ * 4. Can exception be secified multiple times?
+ */
+public class JavaExceptionQuestion {
+
+    public void method() throws /*NullPointerException,*/ IOException /*, IOException*/ {
+    }
+}
---
+++ questions/src/question2/StackOverflowErrorQuestion.java
@@ -0,0 +1,13 @@
+package question2;
+
+/**
+ * 1. What is a StackOverflowError?
+ * 2. Create an example that throws a StackOverflowError.
+ * 3. How to deal with the StackOverflowError
+ */
+public class StackOverflowErrorQuestion {
+
+    public static void main(String[] args) {
+
+    }
+}
